0be0a4888d1b782ef71a78611dbbe3d0dde1578e
Sam Erickson <samericksn@gmail.com>
2020-07-01 14:06:47 -0700

    Clean up default files

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <NavBar />
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    NavBar,
+  },
+};
+</script>
 
 <style>
 #app {
