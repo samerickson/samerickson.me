424bc07a040655f1811ceea47a9590dfd231da8d
Sam Erickson <samericksn@gmail.com>
2020-07-03 11:43:12 -0700

    Add tooltips to header icons

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
   <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand>Sam Erickson</b-navbar-brand>
+    <b-container>
+      <b-navbar-brand>Sam Erickson</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
@@ -14,20 +14,33 @@
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
+      <b-navbar-nav class=".col-md-4 ml-auto">
         <b-navbar-brand href="https://github.com/samerickson">
-          <img class='header-icon' src="../assets/icons/github.svg" alt="Github icon">
+          <b-tooltip target="github-tooltip" triggers="hover">Visit my Github Page</b-tooltip>
+          <img
+            class='header-icon'
+            src="@/assets/icons/github.svg"
+            alt="Github icon"
+            id="github-tooltip"
+          />
         </b-navbar-brand>
         <b-navbar-brand href="https://dev.to/samerickson">
-          <img class='header-icon' src="../assets/icons/dev-icon.svg" alt="Dev.to icon">
+          <b-tooltip target="blog-tooltip" triggers="hover">Visit my blog</b-tooltip>
+          <img
+          class='header-icon'
+          src="@/assets/icons/dev-icon.svg"
+          alt="Dev.to icon"
+          id="blog-tooltip"
+          >
         </b-navbar-brand>
         <b-navbar-brand href="mailto:contact@samerickson.me">
-          <img class='header-icon' src="../assets/icons/mail.svg" alt="Mail icon">
+          <b-tooltip target="mail-tooltip" triggers="hover">Send me an email</b-tooltip>
+          <img class='header-icon' src="@/assets/icons/mail.svg" alt="Mail icon" id="mail-tooltip">
         </b-navbar-brand>
       </b-navbar-nav>
     </b-collapse>
+    </b-container>
   </b-navbar>
-</div>
 </template>
 
 <style lang="css" scoped>
