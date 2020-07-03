5e9a556c42ecb2f62c10e0073067ea123fb8b1dd
Sam Erickson <samericksn@gmail.com>
2020-07-03 11:47:00 -0700

    Add Jumbotron to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="home">
-    <h1>This is the home Page</h1>
-  </div>
+  <section>
+    <b-jumbotron>
+      <b-container>
+        <h1 class="display-3">Sam Erickson</h1>
+        <p class="lead">Welcome to my webpage</p>
+        <p>I am a computer science student at Vancouver Island University.</p>
+        <b-button to="/about" class="mr-2" variant="primary" href="#">Learn More</b-button>
+        <b-button to="/projects" variant="success" href="#">See Projects</b-button>
+        </b-container>
+    </b-jumbotron>
+    <Skills />
+    <CssArt />
+  </section>
 </template>
+
+<script>
+
+export default {
+  components: {
+  },
+};
+</script>
