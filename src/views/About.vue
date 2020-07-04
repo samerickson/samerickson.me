06f391b3e94b54b132c6299cace7fb50d033ed17
Sam Erickson <samericksn@gmail.com>
2020-07-04 09:05:22 -0700

    Add about bio

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <b-container>
+    <h1 class="text-center my-5">About me</h1>
+    <b-row>
+      <b-col cols=8 offset-md="2">
+        <p>
+          Sam is currently a computer science student at
+          <a href="https://www.viu.ca/programs/science-and-technology/computer-science-bsc">
+            Vancouver Island University,
+          </a>
+          where he also teaches programming and computer science to fifth and sixth grade
+          kids in collaberation with the <a href="https://www.uvic.ca/">University of Victoria</a>
+          and <a href="https://www.scienceventure.ca/">Science Venture.</a>
+          You can learn more about the Nanaimo coding club <a href="https://www.scienceventure.ca/nanaimo-coding-club">here.</a>
+        </p>
+        <p>
+          Passionate about containers, virtualization, and operating systems, Sam spends
+          most of his time tinkering with servers.
+        </p>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
