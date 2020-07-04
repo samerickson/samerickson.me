5fbf9c0029c194edddc273b5d0fdd29b6d1b7430
Sam Erickson <samericksn@gmail.com>
2020-07-03 19:17:44 -0700

    Add image and scroll down icon to main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,23 @@
   <section>
     <b-jumbotron>
       <b-container>
-        <h1 class="display-3">Sam Erickson</h1>
-        <p class="lead">Welcome to my webpage</p>
-        <p>I am a computer science student at Vancouver Island University.</p>
-        <b-button to="/about" class="mr-2" variant="primary" href="#">Learn More</b-button>
-        <b-button to="/projects" variant="success" href="#">See Projects</b-button>
+        <b-row align-v="center">
+          <b-col>
+            <h1 class="display-3">Sam Erickson</h1>
+            <p class="lead">Welcome to my webpage</p>
+            <p>I am a computer science student at Vancouver Island University.</p>
+            <b-button to="/about" class="mr-2" variant="primary" href="#">Learn More</b-button>
+            <b-button to="/projects" variant="success" href="#">See Projects</b-button>
+          </b-col>
+          <b-col>
+            <b-img src="https://openclipart.org/image/2400px/svg_to_png/194077/Placeholder.png" fluid></b-img>
+          </b-col>
+        </b-row>
       </b-container>
     </b-jumbotron>
+    <div class="text-center">
+      <b-icon animation="cylon-vertical" icon="chevron-double-down" font-scale="3.2"></b-icon>
+    </div>
     <Skills />
     <CssArt />
   </section>
