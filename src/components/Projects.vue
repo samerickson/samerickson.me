250486af9d016bb98e459666a91816637f0eac6a
Sam Erickson <samericksn@gmail.com>
2020-07-04 16:06:34 -0700

    Split projects component from projects view

---
+++ src/components/Projects.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-row>
+    <b-col cols="7">
+      <img src="@/assets/projects/demos/craps_demo.gif" width="600px" />
+    </b-col>
+    <b-col>
+      <h2>Craps.py</h2>
+      <small class="secondary">Date: 01/01/2020</small>
+      <p>
+        This project was created to determine the average wins and losses that
+        will result in a set number of craps games, and what the longest win
+        and lose streaks would be.
+      </p>
+      <p>
+        The plan is to be able to specify <code>startingCash</code>
+        and completely simulate a game with a predetermined betting
+        strategy and print the output to the user.
+      </p>
+    </b-col>
+  </b-row>
+</template>
