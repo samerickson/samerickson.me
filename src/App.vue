2a9ec203ad5c0f94e99410edc9983b83a481d942
Sam Erickson <samericksn@gmail.com>
2020-07-04 17:35:53 -0700

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
     <NavBar />
-    <router-view/>
+    <div class="content">
+      <router-view/>
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import NavBar from '@/components/NavBar.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
   name: 'Home',
   components: {
     NavBar,
+    Footer,
   },
 };
 </script>
+
+<style scoped>
+ .content {
+   min-height: 90vh;
+ }
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer id="footer">
+    <b-container class="footer-wrap pt-5">
+      <b-row>
+        <b-col class="text-left">
+            <strong>Social Media Links</strong><br>
+            <a href="https://www.hackerrank.com/samericksn">Hacker Rank</a><br>
+            <a href="https://github.com/samerickson">Github</a><br>
+            <a href="https://codepen.io/samerickson">Codepen</a><br>
+            <a href="https://dev.to/samerickson">Dev.to</a><br>
+        </b-col>
+        <b-col class="text-center">
+          <strong>Contact</strong><br>
+          <router-link tag="a" to="/contact">Contact Page</router-link><br>
+          <a href="mailto:contact@samerickson.me">Send me an email</a><br>
+        </b-col>
+        <b-col class="text-right">
+          <strong>Resources</strong><br>
+          <a href="https://feathericons.com/">Feather Icons</a><br>
+          <a href="https://simpleicons.org/">Simple Icons</a><br>
+          <a href="https://bootstrap-vue.org/">Bootstrap-Vue</a><br>
+          <a href="https://vuejs.org/">Vue.js</a>
+        </b-col>
+      </b-row>
+      <p class="text-center">Copyright 2020 Sam Erickson</p>
+    </b-container>
+  </footer>
+</template>
+
+<style scoped>
+  #footer {
+    background-color: #e9ecef;
+    overflow: hidden;
+  }
+</style>
