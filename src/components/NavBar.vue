055ecb815c167123d32584bc00debaff308395ce
Sam Erickson <samericksn@gmail.com>
2020-07-04 18:04:18 -0700

    Add css artwork view

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,6 +9,7 @@
       <b-navbar-nav>
         <b-nav-item to="/">Home</b-nav-item>
         <b-nav-item to="/projects">Projects</b-nav-item>
+        <b-nav-item to="/css-art">Css Artwork</b-nav-item>
         <b-nav-item to="/contact">Contact</b-nav-item>
         <b-nav-item to="/about">About</b-nav-item>
       </b-navbar-nav>
---
+++ src/views/CssArt.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-container class="mt-5">
+    <h1 class="my-4">Css Artwork</h1>
+    <CssArt />
+  </b-container>
+</template>
+
+<script>
+import CssArt from '@/components/CssArt.vue';
+
+export default {
+  components: {
+    CssArt,
+  },
+};
+</script>
