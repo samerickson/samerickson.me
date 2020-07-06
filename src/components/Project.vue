91ac011132dd28c62789d738602ac574eb0d3e7d
Sam Erickson <samericksn@gmail.com>
2020-07-06 11:59:53 -0700

    Update project handling

---
+++ src/components/Project.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-row>
+    <b-col lg="7" md="12">
+      <img :src="require('@/assets/projects/demos/' + demo + '')" width="100%" />
+    </b-col>
+    <b-col lg="5" md="12">
+      <h3 class="mt-4 mt-lg-0 mt-md-5 mt-sm-5">{{title}}</h3>
+      <small class="secondary">Date: {{date}}</small>
+      <slot></slot>
+      <b-button :href="github">View on Github</b-button>
+    </b-col>
+  </b-row>
+</template>\
+
+<script>
+export default {
+  props: [
+    'demo',
+    'title',
+    'date',
+    'github',
+  ],
+};
+</script>
--- src/components/ProjectHighlights.vue
+++ src/components/ProjectHighlights.vue
@@ -1,22 +1,31 @@
 <template>
-  <b-row>
-    <b-col lg="7" md="12">
-      <img src="@/assets/projects/demos/craps_demo.gif" width="100%" />
-    </b-col>
-    <b-col lg="5" md="12">
-      <h3 class="mt-4 mt-lg-0 mt-md-5 mt-sm-5">Craps.py</h3>
-      <small class="secondary">Date: 01/01/2020</small>
-      <p>
-        This project was created to determine the average wins and losses that
-        will result in a set number of craps games, and what the longest win
-        and lose streaks would be.
-      </p>
-      <p>
-        The plan is to be able to specify <code>startingCash</code>
-        and completely simulate a game with a predetermined betting
-        strategy and print the output to the user.
-      </p>
-      <b-button href="https://github.com/samerickson/craps">View on Github</b-button>
-    </b-col>
-  </b-row>
+  <div>
+    <Project
+      demo="craps_demo.gif"
+      title="Craps Simulator"
+      date="June 2020"
+      github="https://github.com/samerickson/craps"
+    >
+        <p>
+          This project was created to determine the average wins and losses that
+          will result in a set number of craps games, and what the longest win
+          and lose streaks would be.
+        </p>
+        <p>
+          The plan is to be able to specify <code>startingCash</code>
+          and completely simulate a game with a predetermined betting
+          strategy and print the output to the user.
+        </p>
+    </Project>
+  </div>
 </template>
+
+<script>
+import Project from '@/components/Project.vue';
+
+export default {
+  components: {
+    Project,
+  },
+};
+</script>
