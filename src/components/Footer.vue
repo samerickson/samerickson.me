4efb76960fb1389b02fa4be641948fcb3ad93f61
Sam Erickson <samericksn@gmail.com>
2020-07-06 11:28:51 -0700

    Make footer responsive

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,19 +2,19 @@
   <footer id="footer">
     <b-container class="footer-wrap pt-5">
       <b-row>
-        <b-col class="text-left">
-            <strong>Social Media Links</strong><br>
+        <b-col class="text-center text-md-left" cols="12" md="4">
+            <strong>Social Links</strong><br>
             <a href="https://www.hackerrank.com/samericksn">Hacker Rank</a><br>
             <a href="https://github.com/samerickson">Github</a><br>
             <a href="https://codepen.io/samerickson">Codepen</a><br>
             <a href="https://dev.to/samerickson">Dev.to</a><br>
         </b-col>
-        <b-col class="text-center">
+        <b-col class="text-center my-4 my-md-0" cols="12" md="4">
           <strong>Contact</strong><br>
           <router-link tag="a" to="/contact">Contact Page</router-link><br>
           <a href="mailto:contact@samerickson.me">Send me an email</a><br>
         </b-col>
-        <b-col class="text-right">
+        <b-col class="text-center text-md-right mb-5 mb-md-0" cols="12" md="4">
           <strong>Resources</strong><br>
           <a href="https://feathericons.com/">Feather Icons</a><br>
           <a href="https://simpleicons.org/">Simple Icons</a><br>
