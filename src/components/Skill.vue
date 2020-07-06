a3039c7134de7a2b0f39a9aee9bd804a4355d18a
Sam Erickson <samericksn@gmail.com>
2020-07-06 11:01:09 -0700

    Add skill component

---
+++ src/components/Skill.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-row class="my-4">
+    <b-col md="4" cols="12">
+      <a href=""><h4>{{skill}}</h4></a>
+    </b-col>
+    <b-col md="8" cols="12">
+      <b-form-rating v-model="value" readonly></b-form-rating>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  props: [
+    'skill',
+    'rating',
+  ],
+  data() {
+    return {
+      value: parseFloat(this.rating),
+    };
+  },
+};
+</script>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,107 +1,23 @@
 <template>
   <b-container>
     <h2 class="my-5">Skills</h2>
-
-    <b-row>
-      <b-col cols="4">
-        <h3>Technology</h3>
-      </b-col>
-      <b-col cols="8">
-        <h3>Skill level</h3>
-      </b-col>
-    </b-row>
-    <hr>
-    <!-- The below skills should probably be a component of their own -->
-        <!-- Linux -->
-    <b-row class="my-4">
-      <b-col cols="4">
-        <a href=""><h4>Linux</h4></a>
-      </b-col>
-      <b-col cols="8">
-        <b-form-rating v-model="linux" readonly></b-form-rating>
-      </b-col>
-    </b-row>
-
-    <!-- Virtual Machines -->
-    <b-row class="my-4">
-      <b-col cols="4">
-        <a href=""><h4>Virtual Machines</h4></a>
-      </b-col>
-      <b-col cols="8">
-        <b-form-rating v-model="virtualMachines" readonly></b-form-rating>
-      </b-col>
-    </b-row>
-
-    <!-- C -->
-    <b-row class="my-4">
-      <b-col cols="4">
-        <a href=""><h4>C</h4></a>
-      </b-col>
-      <b-col cols="8">
-        <b-form-rating v-model="c" readonly></b-form-rating>
-      </b-col>
-    </b-row>
-
-    <!-- Nodejs -->
-    <b-row class="my-4">
-      <b-col cols="4">
-        <a href="">
-          <h4>Node.js</h4>
-        </a>
-      </b-col>
-      <b-col cols="8">
-        <b-form-rating v-model="nodeJs" readonly></b-form-rating>
-      </b-col>
-    </b-row>
-
-    <!-- Javascript -->
-    <b-row class="my-4">
-      <b-col cols="4">
-        <a href=""><h4>Javascript</h4></a>
-      </b-col>
-      <b-col cols="8">
-        <b-form-rating v-model="javascript" readonly></b-form-rating>
-      </b-col>
-    </b-row>
-
-    <!-- Python -->
-    <b-row class="my-4">
-      <b-col cols="4">
-        <a href=""><h4>Python</h4></a>
-      </b-col>
-      <b-col cols="8">
-        <b-form-rating v-model="python" readonly></b-form-rating>
-      </b-col>
-    </b-row>
-
-    <!-- Vue -->
-    <b-row class="my-4">
-      <b-col cols="4">
-        <a href="">
-          <h4>Vue</h4>
-        </a>
-      </b-col>
-      <b-col cols="8">
-        <b-form-rating v-model="vue" readonly></b-form-rating>
-      </b-col>
-    </b-row>
-
+    <Skill skill="Virtual Machines" rating="5"/>
+    <Skill skill="Javascript" rating="4.5"/>
+    <Skill skill="Linux" rating="4"/>
+    <Skill skill="Python" rating="3.5"/>
+    <Skill skill="NodeJS" rating="4"/>
+    <Skill skill="VueJS" rating="4"/>
+    <Skill skill="C" rating="3.5"/>
   </b-container>
 
 </template>
 
 <script>
+import Skill from '@/components/Skill.vue';
+
 export default {
-  data() {
-    return {
-      c: 3,
-      javascript: 3,
-      python: 3.5,
-      linux: 4.5,
-      virtualMachines: 5,
-      vue: 3,
-      nodeJs: 4,
-    };
+  components: {
+    Skill,
   },
 };
 </script>
