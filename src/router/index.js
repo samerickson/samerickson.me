048717129120df6636553e215f18c63ed3ee1ba5
Sam Erickson <samericksn@gmail.com>
2020-07-06 19:02:30 -0700

    Add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -42,6 +42,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/404',
+    alias: '*',
+    component: () => import('../views/NotFound.vue'),
+  },
 ];
 
 const router = new VueRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <b-container class="my-5 text-center">
+    <h1 class="display-1">404</h1>
+    <h2 class="text-secondary" >The page you are looking for could not be found</h2>
+    <b-button variant="success" class="my-4" to="/">Go to safety</b-button>
+  </b-container>
+</template>
